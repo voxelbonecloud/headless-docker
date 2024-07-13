42890e44312569605e427ac172ccd35c0e67793f
bredo <bredo@bredo.tech>
2024-07-13 19:44:53 +1200

    scripts: remove excess line in launch-resonite.sh

--- scripts/launch-resonite.sh
+++ scripts/launch-resonite.sh
@@ -6,5 +6,3 @@ find /Logs -type f -name *.log -atime +${LOG_RETENTION:-30} -delete
 
 cd /home/container/Headless/net8.0
 dotnet Resonite.dll -HeadlessConfig /Config/${CONFIG_FILE} -Logs /Logs/ ${ADDITIONAL_ARGUMENTS}
-
-#dotnet /home/container/Headless/net8.0/Resonite.dll -HeadlessConfig /Config/${CONFIG_FILE} -Logs /Logs/ ${ADDITIONAL_ARGUMENTS}
