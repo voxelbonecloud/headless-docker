7e79c393e437d0c8523c5b1a81e8379952a7aca5
Sveken <sveken@live.com.au>
2024-07-15 09:44:46 +1000

    Add Modding Support

--- scripts/launch-resonite.sh
+++ scripts/launch-resonite.sh
@@ -5,4 +5,8 @@ rm -r /home/container/Headless/Cache
 find /Logs -type f -name *.log -atime +${LOG_RETENTION:-30} -delete
 
 cd /home/container/Headless/net8.0
+if [${ENABLE_MODS} = true]; then
+dotnet Resonite.dll -HeadlessConfig /Config/${CONFIG_FILE} -Logs /Logs/ -LoadAssembly Libraries/ResoniteModLoader.dll ${ADDITIONAL_ARGUMENTS}
+else
 dotnet Resonite.dll -HeadlessConfig /Config/${CONFIG_FILE} -Logs /Logs/ ${ADDITIONAL_ARGUMENTS}
+fi
