be4c22c27b9ae929bf97ee44812100ec23764153
ProbablePrime <richard.fox@ahref.co.uk>
2024-07-15 15:53:26 -0700

    fix(update): remove no longer needed migrations thingy

--- scripts/update-resonite.sh
+++ scripts/update-resonite.sh
@@ -4,9 +4,6 @@ HEADLESS_DIRECTORY="/home/container/Headless/net8.0"
 
 /home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update 2519830 -beta ${STEAM_BRANCH} -betapassword ${BETA_CODE} +quit
 
-# Fix for issue with NVR.json migrations exploding
-ln -sf /home/container/Migrations /home/container/Headless/Migrations
-
 #Mod installation if ENABLE_MODS is true. Heavily inspired and pulled from work by Spex. Thank you
 if [ "${ENABLE_MODS}" = "true" ]; then
 
